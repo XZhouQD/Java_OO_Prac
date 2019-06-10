8eaf0364150aa5cf123a3c85e45fdea0c42216ec
Xiaowei Zhou <z5108173@student.unsw.edu.au>
2019-06-10 12:17:43 +1000

    adding structure

--- OO_prac/src/prac01/Main.java
+++ OO_prac/src/prac01/Main.java
@@ -1,12 +1,26 @@
 package prac01;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		int a = 5;
-		double b = 3.65;
-		System.out.println("a = " + a + ", b = " + b);
+		
 		System.out.println("Welcome to Java OO Practice!");
+		
+//		int a1 = 5;
+//		double b1 = 3.65;
+//		System.out.println("a = " + a1 + ", b = " + b1);
+		
+//		Variable vars = new Variable();
+//		System.out.println("improted: "+ vars.a +/* ", " + vars.b*/ /*+ ", " + vars.c*/ /*+ ", " + vars.d*/);
+		
+//		Variable var1 = new Variable();
+//		var1.e += 1;
+//		var1.a += 1;
+//		System.out.println("e = " + var1.e + ", a = " + var1.a);
+//		Variable var2 = new Variable();
+//		var2.e += 1;
+//		var2.a += 1;
+//		System.out.println("e = " + var2.e + ", a = " + var2.a);
 	}
-
+	
 }
---
+++ OO_prac/src/prac01/Variable.java
@@ -0,0 +1,26 @@
+package prac01;
+
+public class Variable {
+	public int a = 5;
+	protected double b = 3.65;
+	char c = 'c';
+	private double d = 1.3;
+	public static int e = 0;
+	
+//	public int a;
+//	protected double b;
+//	char c;
+//	private double d;
+	
+	public Variable() {
+		// Empty Constructor
+	}
+	
+//	public Variable(int a, double b, char c, double d) {
+//		this.a = a;
+//		this.b = b;
+//		this.c = c;
+//		this.d = d;
+//	}
+
+}
