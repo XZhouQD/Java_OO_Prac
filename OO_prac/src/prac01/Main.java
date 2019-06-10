ff951d4985acadb4355ba28b7b55df4f490aeee4
Xiaowei Zhou <z5108173@student.unsw.edu.au>
2019-06-10 11:01:56 +1000

    simple main method

--- OO_prac/src/prac01/Main.java
+++ OO_prac/src/prac01/Main.java
@@ -3,7 +3,10 @@ package prac01;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		int a = 5;
+		double b = 3.65;
+		System.out.println("a = " + a + ", b = " + b);
+		System.out.println("Welcome to Java OO Practice!");
 	}
 
 }
